399980fe4a9f276337978467bd6b9fe26acf3d63
JoakoMasq <joamasquere@hotmail.com>
2023-06-08 20:36:35 -0300

    Se agrego el Login y Register, pero no esta terminado

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.get('/', (req, res) =>{
     res.sendFile((__dirname + '/view/Home.html'));
 });
 
+app.get('/Register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './view/Register.html'));
+});
+
+app.get('/Login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './view/Login.html'));
+});
+
 app.listen(3030, function () {
     console.log('Servidor corriendo');
 });
